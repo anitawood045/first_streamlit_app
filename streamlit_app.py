9278608c15d16b58461c6f1f4c07f5c26f7ac171
Anita Joy <120427994+anitawood045@users.noreply.github.com>
2022-12-13 13:01:43 +1300

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,4 @@ import streamlit
 streamlit.title('my parents dinner')
 streamlit.header('Roger logistics')
 streamlit.text('starting on jan 1 2024')
-stramlit.text('happy birthday roger')
+streamlit.text('happy birthday roger')
